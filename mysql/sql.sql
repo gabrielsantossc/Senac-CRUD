1059d7463462c58294ab148ee5132dd78bc96cfe
gabrielsantossc <gabrielsenac2025@gmail.com>
2025-02-17 16:35:54 -0300

    Add files via upload

--- mysql/sql.sql
+++ mysql/sql.sql
@@ -2,7 +2,16 @@ USE Empresa;
 
 CREATE TABLE funcionarios (
     id int AUTO_INCREMENT PRIMARY KEY,
-    funcionario VARCHAR (60) NOT NULL,
-    cidade VARCHAR (50) NOT NULL,
-    data_contracao DATE NOT NULL
+    cliente VARCHAR (60) NOT NULL,
+    endereco VARCHAR (50) NOT NULL,
+    telefone VARCHAR (50) NOT NULL,
+    cep VARCHAR (50) NOT NULL,
+);
+
+
+CREATE TABLE funcionarios (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    produto VARCHAR (60) NOT NULL,
+    estoque int (50) NOT NULL,
+    preco decimal (15,2) NOT NULL,
 );
